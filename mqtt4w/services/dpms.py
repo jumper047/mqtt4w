0c4aa7a0c34119a1f7bce4cc86c3d2d82606109f
Dmitrii Pshonko <d.pshonko@ngenix.net>
2022-10-31 03:01:36 +0300

    Add DPMS service

---
+++ mqtt4w/services/dpms.py
@@ -0,0 +1,109 @@
+import asyncio
+import ctypes
+import json
+import logging
+import struct
+from enum import IntEnum
+from pathlib import Path
+from typing import AsyncGenerator, List
+
+from mqtt4w.services.common.config import ServiceBaseModel
+from mqtt4w.services.common.constants import OFF, ON
+from mqtt4w.services.common.discovery import (
+    SUBTOPIC_TEMPLATE,
+    DiscoveryEntity,
+    EntityType,
+    generate_availability_config,
+    generate_device_cfg,
+    generate_subconfig,
+)
+from mqtt4w.services.common.structures import Message
+from pydantic import PositiveInt
+
+from .common import AbstractService
+
+LIBXEXT_NAME = "libXext.so"
+
+
+LOG = logging.getLogger(__name__)
+
+
+class DPMS(IntEnum):
+    NONE = -2
+    FAIL = -1
+    ON = 0
+    STANDBY = 1
+    SUSPEND = 2
+    OFF = 3
+
+
+class DPMSService(AbstractService):
+    def __init__(self, subtopic, display, check_interval):
+        self.incoming_msg = asyncio.Queue()
+        self.outgoing_msg = asyncio.Queue()
+        self.subtopic = subtopic
+        self.state_topic = subtopic / "state"
+        self.display = display
+        self.libXext = self.get_libxext()
+        self.check_interval = check_interval
+
+    def get_libxext(self):
+        try:
+            ctypes.cdll.LoadLibrary(LIBXEXT_NAME)
+            return ctypes.CDLL(LIBXEXT_NAME)
+        except OSError:
+            LOG.error("Please install libXext package!")
+
+    def dpms_state(self):
+        state = 1
+        display = self.display.encode("ascii")
+        display_name = ctypes.c_char_p()
+        display_name.value = display
+        self.libXext.XOpenDisplay.restype = ctypes.c_void_p  # type: ignore
+        display = ctypes.c_void_p(self.libXext.XOpenDisplay(display_name))  # type: ignore
+        dummy1_i_p = ctypes.create_string_buffer(8)
+        dummy2_i_p = ctypes.create_string_buffer(8)
+        if display.value:
+            if self.libXext.DPMSQueryExtension(  # type: ignore
+                display, dummy1_i_p, dummy2_i_p
+            ) and self.libXext.DPMSCapable(  # type: ignore
+                display
+            ):
+                onoff_p = ctypes.create_string_buffer(1)
+                state_p = ctypes.create_string_buffer(2)
+                if self.libXext.DPMSInfo(display, state_p, onoff_p):  # type: ignore
+                    onoff = struct.unpack("B", onoff_p.raw)[0]
+                    if onoff:
+                        state = struct.unpack("H", state_p.raw)[0]
+            self.libXext.XCloseDisplay(display)  # type: ignore
+        return state
+
+    def discovery(self) -> List[DiscoveryEntity]:
+        dpms_id = "dpms_state"
+        subconfig = generate_subconfig(
+            "DPMS state",
+            state_topic=str(self.state_topic),
+            payload_on=ON,
+            payload_off=OFF,
+        )
+        return [DiscoveryEntity(EntityType.BINARY_SENSOR, dpms_id, subconfig)]
+
+    async def start(self):
+        state = self.dpms_state()
+        self.outgoing_msg.put_nowait(Message(self.state_topic, OFF if state else ON))
+        while True:
+            new_state = self.dpms_state()
+            if state != new_state:
+                state = new_state
+                self.outgoing_msg.put_nowait(
+                    Message(str(self.state_topic), OFF if state else ON)
+                )
+            await asyncio.sleep(self.check_interval)
+
+
+class ServiceModel(ServiceBaseModel):
+    _constructor = DPMSService
+
+    subtopic: Path = Path("dpms")
+    display: str = ":1"
+    check_interval: PositiveInt = 5
